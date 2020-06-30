8ff3fc572318719c86983ab520e64b89423cbb0e
meanzza123 <33210135+meanzza123@users.noreply.github.com>
2020-06-30 18:44:42 +0700

    fix ios play

--- src/app/game/aunjai/loading-game/loading-game.component.ts
+++ src/app/game/aunjai/loading-game/loading-game.component.ts
@@ -1,37 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'loading-game',
   templateUrl: './loading-game.component.html',
   styleUrls: ['./loading-game.component.scss'],
   animations: [
     trigger('FadeOutAnimation', [
-      state('in', style({ opacity: 0.8, display: 'none' })),
+      state('in', style({ opacity: 0.92, display: 'none' })),
       transition(':enter', [
         style({ opacity: 1 }),
         animate(4000)
       ])
-    ]),
-    trigger('FadeInAnimation', [
-      state('out', style({ opacity: 1, })),
-      transition(':enter', [
-        style({ opacity: 0,display: 'none' }),
-        animate(4001)
-      ])
     ])
+    // ,
+    // trigger('FadeInAnimation', [
+    //   state('out', style({ opacity: 1, })),
+    //   transition(':enter', [
+    //     style({ opacity: 0, display: 'none' }),
+    //     animate(4001)
+    //   ])
+    // ])
   ]
 })
 export class LoadingGameComponent implements OnInit {
-  loading : boolean;
+  loading: boolean;
 
-  constructor() {
-    this.loading = false ; 
-   }
+  constructor(private router: Router) {
+    this.loading = false;
+
+  }
 
   ngOnInit() {
     setTimeout(() => {
       this.loading = true;
+      setTimeout(() => {
+        this.router.navigateByUrl('/tutorial');
+      }, 2000);
     }, 2000);
   }
 
