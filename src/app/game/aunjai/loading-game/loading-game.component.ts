d18ec9a1a08f3bb35ef194bb74e420b60a8e14e8
meanzza123 <33210135+meanzza123@users.noreply.github.com>
2020-07-01 16:53:18 +0700

    change bg load

--- src/app/game/aunjai/loading-game/loading-game.component.ts
+++ src/app/game/aunjai/loading-game/loading-game.component.ts
@@ -1,38 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Router } from '@angular/router';
 @Component({
   selector: 'loading-game',
   templateUrl: './loading-game.component.html',
   styleUrls: ['./loading-game.component.scss'],
-  animations: [
-    trigger('FadeOutAnimation', [
-      state('in', style({ opacity: 0.97, display: 'none' })),
-      transition(':enter', [
-        style({ opacity: 1 }),
-        animate(4000)
-      ])
-    ])
-    // ,
-    // trigger('FadeInAnimation', [
-    //   state('out', style({ opacity: 1, })),
-    //   transition(':enter', [
-    //     style({ opacity: 0, display: 'none' }),
-    //     animate(4001)
-    //   ])
-    // ])
-  ]
 })
 export class LoadingGameComponent implements OnInit {
   loading: boolean;
 
-  constructor( private router: Router,) {
-    this.loading = false ; 
-   }
+  constructor(private router: Router,) {
+    this.loading = false;
+  }
 
   ngOnInit() {
-  
-    if (!sessionStorage.getItem('mobileId') ) {
+
+    if (!sessionStorage.getItem('mobileId')) {
       this.router.navigateByUrl('/');
       // this.router.navigateByUrl('/popupError');
       return
@@ -43,6 +25,7 @@ export class LoadingGameComponent implements OnInit {
         this.router.navigateByUrl('/tutorial');
       }, 2000);
     }, 2000);
+
   }
 
 }
