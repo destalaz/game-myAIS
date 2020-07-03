a1bb51754430e716a9f63a3d127bff8cda6684f1
patcharapon-lesing <57524488+patcharapon-lesing@users.noreply.github.com>
2020-07-03 11:45:57 +0700

    Add service check playerComplete

--- src/app/game/aunjai/popup-continue/popup-continue.component.ts
+++ src/app/game/aunjai/popup-continue/popup-continue.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GameService } from 'src/app/service/game.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'popup-continue',
   templateUrl: './popup-continue.component.html',
@@ -12,33 +13,44 @@ export class PopupContinueComponent implements OnInit {
   mobileId: string;
   playId: string
   winnerStatus: boolean
+  playerComplete: boolean;
 
-  constructor(private gameService: GameService) {
+  constructor(private gameService: GameService ,   private router: Router) {
     this.load = false;
-    this.reward = localStorage.getItem('rewardpoint');
-    this.mobileId = sessionStorage.getItem('mobileId');
-    this.playId = sessionStorage.getItem('playId');
+    this.reward = ""
+
   }
 
 
   ngOnInit() {
     this.load = true;
-    this.servedPlayReward(this.mobileId, this.playId);
-
+    this.playerComplete = Boolean(sessionStorage.getItem('playerComplete'));
+    this.reward = localStorage.getItem('rewardpoint');
+    this.checkPlayerComplete();
+    
   }
+
   openPage() {
     this.open = true;
+    this.router.navigateByUrl('/reward_flip');
+  }
+
+  checkPlayerComplete() {
+    if (this.playerComplete !== true) {
+      this.reward = localStorage.getItem('rewardpoint');
+      this.mobileId = sessionStorage.getItem('mobileId');
+      this.playId = sessionStorage.getItem('playId');
+      // this.servedPlayReward(this.mobileId, this.playId);
+    }
   }
 
   servedPlayReward(mobileId, playId) {
     this.gameService.getReward(mobileId, playId).subscribe(res => {
       if (res["resultCode"] === "20000" && res["data"].status === "20000" && res["data"].description === "SUCCESS") {
-        sessionStorage.removeItem("playId");
+        // sessionStorage.removeItem("playId");
         this.load = false;
       }
     });
-
-
   }
 
 }
