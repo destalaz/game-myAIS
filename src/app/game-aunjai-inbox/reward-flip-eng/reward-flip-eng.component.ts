6030a046424bb6de69fda03d85cbbb3d73eff143
meanzza123 <33210135+meanzza123@users.noreply.github.com>
2020-11-09 14:32:09 +0700

    update_host

--- src/app/game-aunjai-inbox/reward-flip-eng/reward-flip-eng.component.ts
+++ src/app/game-aunjai-inbox/reward-flip-eng/reward-flip-eng.component.ts
@@ -21,34 +21,30 @@ export class RewardFlipEngComponent implements OnInit {
   constructor(private gameService: GameService, private router: Router, private route: ActivatedRoute, private rout: Router) { }
 
   ngOnInit() {
-    this.goTutorialPage = false;
-    this.open = false;
-    localStorage.removeItem('sumcclick');
     localStorage.setItem('countWin', "1");
-    localStorage.setItem('language', "ENG");
 
-    this.langauge = 'ENG';
+    if (localStorage.getItem('language_Params') == 'th') {
+      this.langauge = 'TH';
+      //console.log(this.langauge);
+    } else if (localStorage.getItem('language_Params') == 'en') {
+      this.langauge = 'ENG';
+      //console.log(this.langauge);
+    }
 
-    this.gameService.getMobileId(sessionStorage.getItem('token')).subscribe((res) => {
-      if (res['o'] === true) {
-        sessionStorage.setItem('playerComplete', "true");
-        this.router.navigateByUrl('/popupContinue', { queryParams: { langauge: "ENG" } });
-      }
-    })
 
     this.route.queryParams.subscribe(params => {
       this.openPage = params.openPage;
-      console.log('openPage => ', this.openPage)
     })
-    // if (!this.langauge) {
-    //   this.langauge = 'ENG';
-    // }
 
+
+    this.open = false;
+    localStorage.removeItem('sumcclick');
     if (localStorage.getItem('resumeGame')) {
       this.open = true;
     }
-
+    this.goTutorialPage = false;
   }
+
 
   goTutorial() {
     this.goTutorialPage = true;
@@ -62,22 +58,18 @@ export class RewardFlipEngComponent implements OnInit {
     if (level == 1) {
       localStorage.setItem('aispoint', "1");
       localStorage.setItem('rewardpoint', "20");
+      localStorage.setItem('level', "1");
     } else if (level == 2) {
       localStorage.setItem('aispoint', "2");
       localStorage.setItem('rewardpoint', "50");
+      localStorage.setItem('level', "2");
     } else {
       localStorage.setItem('aispoint', "3");
       localStorage.setItem('rewardpoint', "100");
+      localStorage.setItem('level', "3");
     }
     this.open = true;
     this.changes.emit(this.open);
     localStorage.setItem('level', level);
-  }
-
-  deCode(_data) {
-    var _resData;
-    var decoded = jwtDecode(_data);
-    _resData = decoded;
-    return _resData;
   }
 }
